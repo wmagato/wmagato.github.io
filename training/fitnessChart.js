5ff39ab09f15aec7a5ebf208071b886d605b34af
william magato <william.magato@gmail.com>
2015-03-06 12:57:16 -0500

    logged workout

--- training/fitnessChart.js
+++ training/fitnessChart.js
@@ -41,6 +41,7 @@ nv.addGraph(function() {
                    values: [
                      { x: Date.parse('Jan 02, 2015'), y: 359000 },
                      { x: Date.parse('Jan 09, 2015'), y: 415000 },
+                     { x: Date.parse('Mar 06, 2015'), y: 340000 },
                    ]
                  },
                  {
