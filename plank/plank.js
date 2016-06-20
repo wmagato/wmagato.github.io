7560e242eebdb2de3fbe209d7fd7258d959bec3a
william.magato <bill.magato@intelligrated.com>
2016-06-19 21:59:24 -0400

    adding line chart data

--- plank/plank.js
+++ plank/plank.js
@@ -1,35 +1,54 @@
-    nv.addGraph(function() {
-        chart = nv.models.multiBarChart()
-            .barColor(d3.scale.category20().range())
-            .duration(300)
-            .margin({bottom: 100, left: 70})
-            .rotateLabels(45)
-            .groupSpacing(0.1)
-        ;
-        chart.reduceXTicks(false).staggerLabels(true);
-        chart.xAxis
-            .axisLabel("ID of Furry Cat Households")
-            .axisLabelDistance(35)
-            .showMaxMin(false)
-            .tickFormat(d3.format(',.6f'))
-        ;
-        chart.yAxis
-            .axisLabel("Change in Furry Cat Population")
-            .axisLabelDistance(-5)
-            .tickFormat(d3.format(',.01f'))
-        ;
-        chart.dispatch.on('renderEnd', function(){
-            nv.log('Render Complete');
-        });
-        d3.select('#chart1 svg')
-            .datum(negative_test_data)
-            .call(chart);
-        nv.utils.windowResize(chart.update);
-        chart.dispatch.on('stateChange', function(e) {
-            nv.log('New State:', JSON.stringify(e));
-        });
-        chart.state.dispatch.on('change', function(state){
-            nv.log('state', JSON.stringify(state));
-        });
-        return chart;
-    });
+nv.addGraph(function() {
+    chart = nv.models.lineChart()
+        .options({
+            duration: 300,
+            useInteractiveGuideline: true
+        })
+    ;
+
+    // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
+    chart.xAxis
+        .axisLabel("Days")
+        .tickFormat(d3.format(',.1f'))
+        .staggerLabels(true)
+    ;
+
+    chart.yAxis
+        .axisLabel('Time (s)')
+        .tickFormat(function(d) {
+            if (d == null) {
+                return 'N/A';
+            }
+            return d3.format(',.2f')(d);
+        })
+    ;
+
+    data = plankTimes();
+
+    d3.select('#plank').append('svg')
+        .datum(data)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+function plankTimes() {
+    return [
+        {
+            area: true,
+            key: "Target",
+            values: [
+                { x: 1, y: 20 },
+                { x: 2, y: 20 },
+                { x: 3, y: 30 },
+                { x: 4, y: 30 },
+                { x: 5, y: 40 },
+                { x: 7, y: 45 },
+                { x: 8, y: 45 },
+                { x: 9, y: 60 },
+            ]
+        },
+    ];
+}
