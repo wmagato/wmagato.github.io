6df9cbd4a5c798ec643f787b93323fc493ed923d
william.magato <bill.magato@intelligrated.com>
2016-06-19 21:37:08 -0400

    switched example graph js

--- plank/plank.js
+++ plank/plank.js
@@ -1,33 +1,35 @@
-nv.addGraph(function() {
-    var chart = nv.models.multiBarChart()
-      .transitionDuration(350)
-      .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
-      .rotateLabels(0)      //Angle to rotate x-axis labels.
-      .showControls(false)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
-      .groupSpacing(0.1)    //Distance between each group of bars.
-    ;
-
-    chart.xAxis
-        .tickFormat(d3.format(',f'));
-
-    chart.yAxis
-        .tickFormat(d3.format(',.1f'));
-
-    d3.select('#plank svg')
-        .datum(exampleData())
-        .call(chart);
-
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-});
-
-//Generate some nice data.
-function exampleData() {
-  return stream_layers(3,10+Math.random()*100,.1).map(function(data, i) {
-    return {
-      key: 'Stream #' + i,
-      values: data
-    };
-  });
-}
+    nv.addGraph(function() {
+        chart = nv.models.multiBarChart()
+            .barColor(d3.scale.category20().range())
+            .duration(300)
+            .margin({bottom: 100, left: 70})
+            .rotateLabels(45)
+            .groupSpacing(0.1)
+        ;
+        chart.reduceXTicks(false).staggerLabels(true);
+        chart.xAxis
+            .axisLabel("ID of Furry Cat Households")
+            .axisLabelDistance(35)
+            .showMaxMin(false)
+            .tickFormat(d3.format(',.6f'))
+        ;
+        chart.yAxis
+            .axisLabel("Change in Furry Cat Population")
+            .axisLabelDistance(-5)
+            .tickFormat(d3.format(',.01f'))
+        ;
+        chart.dispatch.on('renderEnd', function(){
+            nv.log('Render Complete');
+        });
+        d3.select('#chart1 svg')
+            .datum(negative_test_data)
+            .call(chart);
+        nv.utils.windowResize(chart.update);
+        chart.dispatch.on('stateChange', function(e) {
+            nv.log('New State:', JSON.stringify(e));
+        });
+        chart.state.dispatch.on('change', function(state){
+            nv.log('state', JSON.stringify(state));
+        });
+        return chart;
+    });
